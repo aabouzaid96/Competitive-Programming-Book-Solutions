22c088a87cca2fafa0e36b60154ffc0a7260c44a
Abdulrahman Abouzaid <aabouzaid96@gmail.com>
2021-04-09 02:05:34 +0200

    Update Arrays-and-strings-solutions.java

--- Cracking-Coding-Interview-Book/Arrays-and-strings-solutions.java
+++ Cracking-Coding-Interview-Book/Arrays-and-strings-solutions.java
@@ -1,4 +1,5 @@
-1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+### 1.1 
+Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
 cannot use additional data structures?
 
 ##Solution 1
@@ -18,14 +19,15 @@ Boolean isUniqueString(String str){
 //time O(n)  space O(0)
 // without additional data structure
 Boolean isUniqueString(String s){
-         int checker = 0;
+        // use int because of it consist of 128 bit, if constraint become bigger we can use long
+        int checker = 0;
         for (char c :s.toCharArray()) {
             int charAcii= c;
-          // check if the bit is true or false
+          // check if the current bit is true or false
             if((checker & (1 << charAcii)) > 0){
                 return false;
             }
-           // toggle the bit is true
+           // set flag to current bit to true
             checker |= ( 1 << charAcii);
         }
     return true;
